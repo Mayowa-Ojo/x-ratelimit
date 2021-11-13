afd1cddf0d08d1f55826809497eac63a35949aeb
Mayowa-Ojo <mayowaojo.e@gmail.com>
2021-11-13 08:47:55 -0700

    feat: implement reset method, mutex for concurrent access

--- ratelimit.go
+++ ratelimit.go
@@ -8,6 +8,7 @@ import (
 	"net"
 	"net/http"
 	"strings"
+	"sync"
 	"time"
 
 	redis "github.com/go-redis/redis/v8"
@@ -17,17 +18,7 @@ const RedisAddr = "localhost:6379"
 
 var ErrRateLimitExceeded = errors.New("client has exceeded rate limit for given period")
 
-type RateLimit struct {
-	Config
-	Store
-}
-
-type Store interface {
-	GetItem(key string) string
-	SetItem()
-	DeleteItem()
-}
-
+// Redis store
 type RedisStore struct {
 	client    *redis.Client
 	namespace string
@@ -91,34 +82,114 @@ func (s *RedisStore) DeleteItem(ctx context.Context, key string) error {
 	return nil
 }
 
-type RequestLog struct {
-	Timestamp time.Time
-	Remaining int
-}
-
-type Config struct {
+type RateLimitConfig struct {
 	Duration  time.Duration
 	Limit     int
-	Store     interface{}
 	Skip      func() bool
 	Whitelist []string
 }
 
-func (c *RateLimit) New(store Store, config Config) {
-
+type RateLimit struct {
+	RateLimitConfig
+	Store
+	m sync.Mutex
 }
 
-func (c *RateLimit) Incr(key string) (*RequestLog, error) {
-	// entry := c.store.GetItem(key)
-	return nil, nil
+type Store interface {
+	GetItem(ctx context.Context, key string) (*RequestLog, error)
+	SetItem(ctx context.Context, key string, payload *RequestLog) error
+	DeleteItem(ctx context.Context, key string) error
 }
 
-func (c *RateLimit) Remaining() {
-
+type RequestLog struct {
+	Timestamp time.Time
+	Counter   int
 }
 
-func (c *RateLimit) Reset() {
+func New(store Store, config RateLimitConfig) *RateLimit {
+	return &RateLimit{
+		RateLimitConfig: config,
+		Store:           store,
+	}
+}
 
+func (c *RateLimit) Consume(ctx context.Context, key string) (*RequestLog, error) {
+	c.m.Lock()
+	defer c.m.Unlock()
+
+	var payload RequestLog
+
+	rlog, err := c.Store.GetItem(ctx, key)
+	if err != nil {
+		// check nil error
+		if strings.Split(err.Error(), ": ")[1] != "nil" {
+			return nil, err
+		}
+
+		payload.Timestamp = time.Now()
+		payload.Counter = 1
+
+		if err := c.Store.SetItem(ctx, key, &payload); err != nil {
+			return nil, err
+		}
+
+		rlog, err := c.Store.GetItem(ctx, key)
+		if err != nil {
+			return nil, err
+		}
+
+		return rlog, nil
+	}
+
+	if time.Since(rlog.Timestamp) >= c.RateLimitConfig.Duration {
+		// Reset counter
+		return c.Reset(ctx, key)
+	}
+
+	if rlog.Counter >= c.RateLimitConfig.Limit {
+		return nil, ErrRateLimitExceeded
+	}
+
+	payload.Timestamp = rlog.Timestamp
+	payload.Counter = rlog.Counter + 1
+
+	if err := c.Store.SetItem(ctx, key, &payload); err != nil {
+		return nil, err
+	}
+
+	rlog, err = c.Store.GetItem(ctx, key)
+	if err != nil {
+		return nil, err
+	}
+
+	return rlog, nil
+}
+
+func (c *RateLimit) Remaining(ctx context.Context, key string) (*int, error) {
+	rlog, err := c.Store.GetItem(ctx, key)
+	if err != nil {
+		return nil, err
+	}
+
+	return &rlog.Counter, nil
+}
+
+func (c *RateLimit) Reset(ctx context.Context, key string) (*RequestLog, error) {
+	var payload RequestLog
+
+	payload.Timestamp = time.Now()
+	payload.Counter = 1
+
+	if err := c.Store.SetItem(ctx, key, &payload); err != nil {
+		return nil, err
+	}
+
+	rlog, err := c.Store.GetItem(ctx, key)
+	if err != nil {
+		return nil, err
+	}
+
+	return rlog, nil
 }
 
 func (c *RateLimit) GetIp(r *http.Request) (string, error) {
@@ -148,13 +219,5 @@ func (c *RateLimit) GetIp(r *http.Request) (string, error) {
 		return netIp.String(), nil
 	}
 
-	return "", errors.New("pp not found")
+	return "", errors.New("ip not found")
 }
-
-// func Test() {
-// 	c := &RateLimit{}
-// 	redis := &RedisStore{}
-// 	config := Config{}
-
-// 	c.New(redis, config)
-// }
